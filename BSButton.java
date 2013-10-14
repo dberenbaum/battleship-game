0b130e0b2115eb9862c3460ede1674553203d898
dberenbaum <dave.berenbaum@gmail.com>
2013-10-14 15:12:28 -0400

    class for game cells/buttons

---
+++ BSButton.java
@@ -0,0 +1,34 @@
+//Class implementing the buttons representing cells on a BShip game grid.
+
+import javax.swing.*;
+
+public class BSButton extends JButton {
+
+ int gridLocation;
+ Ship cellContents = null;
+ boolean guessed = false;
+
+ public void setGridLocation(int l) {
+  gridLocation = l;
+ }
+
+ public int getGridLocation() {
+  return gridLocation;
+ }
+
+ public void setCellContents(Ship s) {
+  cellContents = s;
+ }
+
+ public Ship getCellContents() {
+  return cellContents;
+ }
+
+ public void guess() {
+  guessed = true;
+ }
+
+ public boolean isGuessed() {
+  return guessed;
+ }
+}
